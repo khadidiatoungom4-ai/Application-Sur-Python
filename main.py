a0679a79d85335277e902a4767dcb5eb1beb3e60
KHADIJA NGOM <khadidiatoungom4@gmail.com>
2026-08-03 20:49:59 +0000

    Partie 2: Stuctures de controle: Creation d'un menu interactif

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Partie 1: Types de base, variables, Entrées et sorties
 print("  Les métadonnées d'un dataset  ")
 nom  = input("Nom du projet :  ")
 domaine = input("Domaine : ")
@@ -6,3 +7,31 @@ colonnes = int(input("Nombre de colonnes : "))
 Mo = float(input("Taille en Mo : "))
 format = input("Format (csv/json) : ")
 public = input("Public True/False : ")
+
+# Partie 2 Structure de controle
+while True:
+
+    print("\n====================")
+    print("1. Ajouter un dataset")
+    print("2. Afficher les datasets")
+    print("3. Rechercher")
+    print("4. Quitter")
+    print("====================")
+
+    choix = input("Votre choix : ")
+
+    if choix == "1":
+        print("Ajout d'un dataset")
+
+    elif choix == "2":
+        print("Affichage")
+
+    elif choix == "3":
+        print("Recherche")
+
+    elif choix == "4":
+        print("Au revoir")
+        break
+
+    else:
+        print("Choix invalide")
